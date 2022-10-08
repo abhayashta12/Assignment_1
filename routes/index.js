8c69629e53afa0dd2257520c2e61dcb123bb4f63
Abhay Ashta <aashta2@my.centennialcollege.ca>
2022-10-07 23:00:56 -0400

    Project 1 included

--- routes/index.js
+++ routes/index.js
@@ -18,17 +18,17 @@ router.get('/about', function(req, res, next) {
 
 /* GET Projects. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });
 });
 
 /* GET Services */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('services', { title: 'Services' });
 });
 
 /* GET Contact Us */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact' });
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
